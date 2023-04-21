1a2b4a8f58aeeb5f71b899f665bcd746af35bafc
beba2123 <antenehbeba@gmail.com>
2023-04-21 15:16:52 +0300

    stack intitial commit

---
+++ runner5.java
@@ -0,0 +1,38 @@
+class stack{
+    int data;
+    int stack[] = new int[5];
+    int lastValue=0;
+    public void push(int data){
+        stack[lastValue] = data;
+        lastValue++;
+    }
+    public int pop(){
+        data = stack[lastValue];
+        stack[lastValue]=0;
+       
+        return data;
+    }
+    public void show(){
+        for( int n : stack){
+            System.out.println(n + " ");
+        }
+}
+
+}
+
+
+
+
+public class runner5 {
+    public static void main(String[] args) {
+        stack nums = new stack();
+        nums.push(12);
+        nums.push(24);
+        nums.push(48);
+        nums.push(60);
+
+
+        nums.show();
+    }
+}
+
