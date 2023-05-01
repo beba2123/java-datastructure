6a26fe9cc23083753c58c876cbff524d43606f7b
beba2123 <antenehbeba@gmail.com>
2023-05-01 20:37:13 +0300

    data structure Queue

--- runner8.java
+++ runner8.java
@@ -2,6 +2,11 @@ public class runner8 {
     public static void main(String[] args) {
         Queue Q = new Queue();
         Q.enQueue(5);
+        Q.enQueue(6);
+        Q.enQueue(7);
+        Q.enQueue(8);
+
+        Q.deQueue();
 
         Q.show();
     }
@@ -13,16 +18,23 @@ class Queue{
      int Queue[] = new int[5];
      int size;
      int rear;
-     int front;
+     int front=0;
      public void enQueue(int data){
        Queue[rear]= data;
          rear++;
          size++;
      }
+     public int deQueue(){
+       int data  = Queue[front];
+        front++;
+        size --;
+
+        return data;
+     }
      public void show(){
         System.out.print("elements : " );
          for(int i=0;i<size;i++){
-            System.out.println(Queue[i] + "  ");
+            System.out.print(Queue[i + front] + "  ");
          }
          
      }
