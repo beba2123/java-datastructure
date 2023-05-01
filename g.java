8097dccf41b6a921e3e4626eccd6d84818c3cf74
beba2123 <antenehbeba@gmail.com>
2023-05-01 21:37:29 +0300

    data structure Queue update

---
+++ g.java
@@ -0,0 +1,6 @@
+public class g {
+    public static void main(String[] args) {
+        int j = 7%5;
+        System.out.println(j);
+    }
+}
--- runner8.java
+++ runner8.java
@@ -7,6 +7,10 @@ public class runner8 {
         Q.enQueue(8);
 
         Q.deQueue();
+        Q.deQueue();
+
+        Q.enQueue(18);
+        Q.enQueue(28);
 
         Q.show();
     }
@@ -21,22 +25,25 @@ class Queue{
      int front=0;
      public void enQueue(int data){
        Queue[rear]= data;
-         rear++;
+         rear = (rear + 1)%5;
          size++;
      }
      public int deQueue(){
        int data  = Queue[front];
-        front++;
+        front = (front + 1)%5;
         size --;
 
         return data;
      }
      public void show(){
-        System.out.print("elements : " );
+        System.out.print( "elements : " );
          for(int i=0;i<size;i++){
-            System.out.print(Queue[i + front] + "  ");
+            System.out.print(Queue[(i + front)%5] + "  ");
          }
-         
+         System.out.println("  ");
+         for(int n : Queue ){
+            System.out.print(n + " ");
+         }
      }
      
 }
